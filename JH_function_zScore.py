71514296fce2752dbc1cb84a2d3ae5d48183bae6
jchallqvist <114823563+jchallqvist@users.noreply.github.com>
2023-03-13 18:45:25 +0000

    Add files via upload

---
+++ JH_function_zScore.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 12 13:15:04 2021
+
+@author: Jenny Hallqvist
+"""
+import pandas as pd
+import scipy.stats as ss
+
+def zScore(DF, fCol): # Row = 1, Column = 0
+         
+    features = DF.iloc[:,fCol:].astype(float) # features to z-score
+    zScored = pd.DataFrame(index = DF.index)
+    clinData = DF.iloc[:,:fCol] # clinical data etc
+    
+    i = 0
+    for var in features.columns:
+        z_var = ss.zscore(features[var], axis=0, ddof=1, nan_policy='omit')
+        zScored.insert(i, value = z_var, column = var)
+        i = i + 1
+        
+    zScored_Final = pd.concat([clinData, zScored], axis = 1, ignore_index = False)
+    return zScored_Final
